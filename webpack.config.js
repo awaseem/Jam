5cf595b42323c74eee297ca05dd0cb8c8e3df1bb
Ali Waseem <waseema393@gmail.com>
2016-07-09 23:41:58 -0700

    Added boilerplate code for first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+/*eslint-disable */
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: [
+        "webpack-dev-server/client?http://localhost:8080",
+        "webpack/hot/only-dev-server",
+        "./src/index.js"
+    ],
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: "react-hot!babel"
+        }, {
+            test: /\.css$/, // Only .css files
+            loader: "react-hot!style!css" // Run both loaders
+        }]
+    },
+    resolve: {
+        extensions: ["", ".js", ".jsx"]
+    },
+    output: {
+        path: __dirname + "/dist",
+        publicPath: "/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: "./dist",
+        hot: true
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            DEBUG: true
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./index.html"
+        })
+    ]
+}
